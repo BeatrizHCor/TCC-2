aef85ffc6c74b20cc0f8bc02f93e98bc46a4fd4c
AnitaTeixeira <141066916+AnitaTeixeira@users.noreply.github.com>
2025-03-27 19:18:34 -0300

    correção

--- ClieteBackend/src/app.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import userRoutes from "./routes/clientRoutes";
-import "./config/database";
-const app = express();
-app.use(express.json());
-app.use("/api/users", userRoutes);
-export default app;
--- ClieteBackend/src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from "./app";
-import dotenv from "dotenv";
-dotenv.config();
-app.listen(PORT, () =
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-});
