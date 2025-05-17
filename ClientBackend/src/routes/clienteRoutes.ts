8ab50a441612f5fb73b88406b74d38ba32da5d66
BeatrizHCor <92833059+BeatrizHCor@users.noreply.github.com>
2025-05-17 15:10:53 -0300

    fix: route update cliente

--- ClientBackend/src/routes/clienteRoutes.ts
+++ ClientBackend/src/routes/clienteRoutes.ts
@@ -1,16 +1,18 @@
-import express from 'express';
-import ClienteController from '../controllers/clienteController';
-
+import express from "express";
+import ClienteController from "../controllers/clienteController";
 
 const router = express.Router();
 
-router.get('/cliente/page', ClienteController.getClientesPage);
-router.get('/cliente/all', ClienteController.findAll);
-router.get('/cliente/ID/:id/', ClienteController.findByID);
-router.get('/cliente/email/:email/:salaoId', ClienteController.findByEmailandSalao);
-router.get('/cliente/cpf/:cpf/:salaoId', ClienteController.findByCpfandSalao);
-router.post('/cliente', ClienteController.create);
-router.put('/cliente/update/:id', ClienteController.update);
-router.delete('/cliente/delete/:id', ClienteController.delete);
+router.get("/cliente/page", ClienteController.getClientesPage);
+router.get("/cliente/all", ClienteController.findAll);
+router.get("/cliente/ID/:id/", ClienteController.findByID);
+router.get(
+  "/cliente/email/:email/:salaoId",
+  ClienteController.findByEmailandSalao
+);
+router.get("/cliente/cpf/:cpf/:salaoId", ClienteController.findByCpfandSalao);
+router.post("/cliente", ClienteController.create);
+router.put("/cliente/:id", ClienteController.update);
+router.delete("/cliente/delete/:id", ClienteController.delete);
 
 export default router;
